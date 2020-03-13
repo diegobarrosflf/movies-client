0582d4ed17865025cd880230f0ed09393b2406f6
Diego Barros <diego.barros.flf@gmail.com>
2020-03-12 21:52:52 -0300

    Implementando alto contraste

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -8,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
 
 export class TopBarComponent implements OnInit {
   
+  element = document.getElementsByTagName("body")[0] as HTMLElement;
   constructor() { }
 
   ngOnInit(): void {
-    
+    this.checkHighContrast();
   }  
+
+  checkHighContrast() {
+    if (sessionStorage.getItem("highContrast") && sessionStorage.getItem("highContrast") === "true")
+      this.element.classList.add("high-contrast");
+    else
+      this.element.classList.remove("high-contrast");
+  }
+
+  changeHighContrast() {
+    if (sessionStorage.getItem("highContrast") && sessionStorage.getItem("highContrast") === "true") {
+
+      sessionStorage.setItem("highContrast", "false");
+      this.element.classList.remove("high-contrast");
+    } else {
+      sessionStorage.setItem("highContrast", "true");
+      this.element.classList.add("high-contrast");
+    }
+  }
 
 }
